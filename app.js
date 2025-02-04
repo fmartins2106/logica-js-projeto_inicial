986d24d78d04d64e24a1ca17b06afd207fbc9040
fmartins2106 <fernandom.adm@gmail.com>
2025-02-03 22:51:23 -0300

    deixando o jogo mais facil

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('Jogoa advinhar números');
+let numeroSecreto =5;
+prompt("Escolha um número de 1 a 10");
+
+if (chute == numeroSecreto){
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}
+
+alert("Boas vidas ao nosso site.");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVenda = 50;
+let saldoDisposonivel = 50;
+alert("Erro! digite todos os campos.");
+let menssagemDeErro = "Erro, preencha todos os campos";
+let nome = prompt("Qual o nome do usuario?:");
+let idade = prompt("Qual a idade?");
+if(idade > 18){
+    alert("Pode tirar a carteira de habilitação.");
+}
+
